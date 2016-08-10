3fe59c355dddd430452ac636395c95097bdeed9a
scheshan <5373827@qq.com>
2016-08-10 09:43:49 +0800

    logic for link

---
+++ src/DotNetBlog.Core/Data/Mappings/LinkMapping.cs
@@ -0,0 +1,17 @@
+﻿using DotNetBlog.Core.Entity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetBlog.Core.Data.Mappings
+{
+    public sealed class LinkMapping
+    {
+        public static void Map(EntityTypeBuilder<Link> builder)
+        {
+
+        }
+    }
+}
---
+++ src/DotNetBlog.Core/Entity/Link.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetBlog.Core.Entity
+{
+    public class Link
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string OpenInNewWindow { get; set; }
+    }
+}
